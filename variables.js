5353db9f1b2103d2bd82ca62dc047d4b70a364cc
Chad DeGange <chaddegange@gmail.com>
2021-09-24 11:43:46 -0600

    Complete programspecific challenge

---
+++ variables.js
@@ -0,0 +1,13 @@
+var movie = 'Top Gun';
+var isEntertaining = true;
+
+movie = 'The Breakfast Club';
+var num4 = 4;
+var num6 = 6;
+
+var product = num4 * num6;
+
+var favoriteFoods = ['Pizza', 'Enchiladas', "Fish Taco"];
+
+favoriteFoods.shift();
+console.log(movie.length -1);
