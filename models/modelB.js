e1a98ada0d91ef45b35ea6fffd4c6a4e3a7ccfbb
Chad DeGange <chaddegange@gmail.com>
2021-09-24 11:59:29 -0600

    Add new developer object

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+//Developer
+
+var newDeveloper = {
+  name: 'Chad DeGange',
+  school: 'Turing',
+  age: 36,
+  yearsInSchool: 16,
+  previousProgrammingExperience: false,
+  frontEndProgram: true,
+  supportSystem: ['Michelle', 'Jasmine', 'Lexy'],
+  jobProspects: ['Google', 'Foreflight', 'Strava', 'Jeppessen']
+};
