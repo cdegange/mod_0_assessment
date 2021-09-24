62db60e4b220d48e60168103fdf9391834f8c378
Chad DeGange <chaddegange@gmail.com>
2021-09-24 11:52:36 -0600

    Add famous dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+//Dinosaur
+
+var famousDinosaur = {
+  name: 'Barney',
+  color: 'Purple',
+  weightInLbs: 4216,
+  ageInYrs: 122,
+  famous: true,
+  lovesChildren: true,
+  friends: ['Big Bird', "Chewbaca"],
+  guestAppearances: ['Show 1', 'Show 2']
+};
